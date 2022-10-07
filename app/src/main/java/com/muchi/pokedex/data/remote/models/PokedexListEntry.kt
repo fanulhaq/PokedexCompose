a515df088b2b7f22e7cc3190d11a4e1ce8aedc28
Irfanul Haq <irfanulhaqdocument@gmail.com>
2022-10-07 16:40:43 +0700

    PokedexList

---
+++ app/src/main/java/com/muchi/pokedex/data/remote/models/PokedexListEntry.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c)  - Irfanul Haq
+ */
+
+package com.muchi.pokedex.data.remote.models
+
+data class PokedexListEntry(
+    val pokemonName: String,
+    val imageUrl: String,
+    val number: Int
+)
